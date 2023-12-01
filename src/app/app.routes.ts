d40c423425055e7bba077e0478ebcc4f4c1fb693
kreuzerk <kevin.kreuzer90@icloud.com>
2023-12-01 12:05:45 +0100

    feat: 🎸 generate components and setup deployment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'foo',
+    pathMatch: 'full'
+  },
+  {
+    path: 'foo',
+    loadComponent: () => import('./features/foo/foo.component').then(c => c.FooComponent)
+  },
+  {
+    path: 'bar',
+    loadComponent: () => import('./features/bar/bar.component').then(c => c.BarComponent)
+  },
+  {
+    path: 'baz',
+    loadComponent: () => import('./features/baz/baz.component').then(c => c.BazComponent)
+  }
+];
